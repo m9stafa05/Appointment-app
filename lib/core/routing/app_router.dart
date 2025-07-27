da709a4a463e6b212463e4f4502101b47f63a3c6
m9stafa05 <m9stafa05@gmail.com>
2025-07-27 03:10:52 +0300

    provide the cubit fro ui screen

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,7 +1,10 @@
+import 'package:appointment_app/core/di/dependency_injection.dart';
 import 'package:appointment_app/core/routing/routes.dart';
+import 'package:appointment_app/features/login/logic/cubit/login_cubit.dart';
 import 'package:appointment_app/features/login/ui/login_screen.dart';
 import 'package:appointment_app/features/onboarding/on_boarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route<dynamic> generateRoute(RouteSettings settings) {
@@ -13,7 +16,12 @@ class AppRouter {
           builder: (_) => const OnBoardingScreen(),
         );
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<LoginCubit>(),
+            child: const LoginScreen(),
+          ),
+        );
       default:
         return MaterialPageRoute(
           builder: (_) => const Scaffold(
