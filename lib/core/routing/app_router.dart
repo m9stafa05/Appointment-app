4216b978e3cf7becb1f5f2c01c1355239e4bcde1
m9stafa05 <m9stafa05@gmail.com>
2025-07-29 08:14:07 +0300

    Create Home screen & add the route

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:appointment_app/core/di/dependency_injection.dart';
 import 'package:appointment_app/core/routing/routes.dart';
+import 'package:appointment_app/features/home/ui/home_screen.dart';
 import 'package:appointment_app/features/login/logic/cubit/login_cubit.dart';
 import 'package:appointment_app/features/login/ui/login_screen.dart';
 import 'package:appointment_app/features/onboarding/on_boarding_screen.dart';
@@ -22,6 +23,8 @@ class AppRouter {
             child: const LoginScreen(),
           ),
         );
+      case Routes.homeScreen:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => const Scaffold(
