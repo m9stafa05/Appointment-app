e3c42d88c7cee7727f7188e75dcfe28ccf98620f
m9stafa05 <m9stafa05@gmail.com>
2025-07-29 10:55:56 +0300

    Add Navigation Back to exit

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,11 +1,13 @@
-import 'package:appointment_app/core/helpers/spacing.dart';
+import 'package:appointment_app/features/login/ui/widgets/dont_have_account.dart';
+import 'package:flutter/services.dart';
+
+import '../../../core/helpers/spacing.dart';
 import 'package:appointment_app/core/themes/text_styles.dart';
-import 'package:appointment_app/core/widgets/custom_text_button.dart';
-import 'package:appointment_app/features/login/logic/cubit/login_cubit.dart';
-import 'package:appointment_app/features/login/ui/login_bloc_listener.dart';
-import 'package:appointment_app/features/login/ui/widgets/already_have_account_text.dart';
-import 'package:appointment_app/features/login/ui/widgets/email_and_password.dart';
-import 'package:appointment_app/features/login/ui/widgets/terms_and_conditions.dart';
+import '../../../core/widgets/custom_text_button.dart';
+import '../logic/cubit/login_cubit.dart';
+import 'login_bloc_listener.dart';
+import 'widgets/email_and_password.dart';
+import 'widgets/terms_and_conditions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -15,55 +17,63 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            vertical: 50.h,
-            horizontal: 30.w,
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Welcome Back",
-                  style: TextStyles.font24BlueBold,
-                ),
-                verticalSpacing(8),
-                Text(
-                  "We're excited to have you back, can't wait to see what you've been up to since you last logged in.",
-                  style: TextStyles.font14GrayRegular.copyWith(
-                    height: 2,
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (bool didPop, dynamic result) {
+        if (!didPop) {
+          SystemNavigator.pop(); // Exit the app gracefully
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 50.h,
+              horizontal: 30.w,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Welcome Back",
+                    style: TextStyles.font24BlueBold,
                   ),
-                ),
-                verticalSpacing(36),
-                Column(
-                  children: [
-                    const EmailAndPassword(),
-                    Align(
-                      alignment: AlignmentDirectional.centerEnd,
-                      child: Text(
-                        'Forgot Password?',
-                        style: TextStyles.font12BlueRegular,
+                  verticalSpacing(8),
+                  Text(
+                    "We're excited to have you back, can't wait to see what you've been up to since you last logged in.",
+                    style: TextStyles.font14GrayRegular.copyWith(
+                      height: 2,
+                    ),
+                  ),
+                  verticalSpacing(36),
+                  Column(
+                    children: [
+                      const EmailAndPassword(),
+                      Align(
+                        alignment: AlignmentDirectional.centerEnd,
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyles.font12BlueRegular,
+                        ),
                       ),
-                    ),
-                    verticalSpacing(36),
-                    CustomTextButton(
-                      buttonText: 'Login',
-                      textStyle: TextStyles.font16WhiteSemiBold,
-                      onPressed: () {
-                        validateAndLogin(context);
-                      },
-                    ),
-                    verticalSpacing(30),
-                    const TermsAndConditions(),
-                    verticalSpacing(100),
-                    const AlreadyHaveAccountText(),
-                    const LoginBlocListener(),
-                  ],
-                ),
-              ],
+                      verticalSpacing(36),
+                      CustomTextButton(
+                        buttonText: 'Login',
+                        textStyle: TextStyles.font16WhiteSemiBold,
+                        onPressed: () {
+                          validateAndLogin(context);
+                        },
+                      ),
+                      verticalSpacing(20),
+                      const TermsAndConditions(),
+                      verticalSpacing(30),
+                      const DontHaveAccount(),
+                      const LoginBlocListener(),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/features/onboarding/on_boarding_screen.dart
+++ lib/features/onboarding/on_boarding_screen.dart
@@ -3,6 +3,7 @@ import 'package:appointment_app/features/onboarding/widgets/doc_log_and_name.dar
 import 'package:appointment_app/features/onboarding/widgets/doctor_image_and_text.dart';
 import 'package:appointment_app/features/onboarding/widgets/get_started_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnBoardingScreen extends StatelessWidget {
@@ -10,31 +11,39 @@ class OnBoardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.only(top: 30.h, bottom: 30.h),
-            child: Column(
-              children: [
-                const DocLogAndName(),
-                SizedBox(height: 30.h),
-                const DoctorImageAndText(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 30.w),
-                  child: Column(
-                    children: [
-                      Text(
-                        textAlign: TextAlign.center,
-                        'Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.',
-                        style: TextStyles.font12GrayRegular,
-                      ),
-                      SizedBox(height: 30.h),
-                      const GetStartedButton(),
-                    ],
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (bool didPop, dynamic result) {
+        if (!didPop) {
+          SystemNavigator.pop();
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(top: 30.h, bottom: 30.h),
+              child: Column(
+                children: [
+                  const DocLogAndName(),
+                  SizedBox(height: 30.h),
+                  const DoctorImageAndText(),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30.w),
+                    child: Column(
+                      children: [
+                        Text(
+                          textAlign: TextAlign.center,
+                          'Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.',
+                          style: TextStyles.font12GrayRegular,
+                        ),
+                        SizedBox(height: 30.h),
+                        const GetStartedButton(),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
