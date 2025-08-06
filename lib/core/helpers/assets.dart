26b022d1ac6f75c3d01e92bf80a59b9ccefd3283
m9stafa05 <m9stafa05@gmail.com>
2025-08-06 04:59:07 +0300

    Add Images To assets

--- lib/core/helpers/assets.dart
+++ lib/core/helpers/assets.dart
@@ -6,4 +6,8 @@ class AssetsManger {
       'assets/images/onboarding_doctor.png';
   static const String notificationIcon =
       'assets/svgs/notfication_icon.svg';
+  static const String doctorsBlueContainer =
+      'assets/images/home_background.png';
+  static const String femaleDoctor =
+      'assets/images/female_doctor.png';
 }
