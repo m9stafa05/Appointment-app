1412001d47f3a97cd4ab2ef699d925ea45563c3e
m9stafa05 <m9stafa05@gmail.com>
2025-08-07 08:39:33 +0300

    Create Api request For home specializations Data And Doctors Data & Build Home Repo For implement the data

--- lib/core/networking/dio_factory.dart
+++ lib/core/networking/dio_factory.dart
@@ -15,11 +15,20 @@ class DioFactory {
       dio!
         ..options.connectTimeout = timeOut
         ..options.receiveTimeout = timeOut;
+      addDioHeader();
       addDioInterceptor();
       return dio!;
     } else {
       return dio!;
     }
+  }
+
+  static void addDioHeader() {
+    dio?.options.headers = {
+      "Accept": "application/json",
+      "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3ZjYXJlLmludGVncmF0aW9uMjUuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNzU0NTI1NjQ3LCJleHAiOjE3NTQ2MTIwNDcsIm5iZiI6MTc1NDUyNTY0NywianRpIjoiV3k5Q0ZQWjk5Tm1Yc3hMTiIsInN1YiI6IjQ2NzUiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.CC0iDWL_Qr4LMigp-zbmth_2hFexLPNk72NDnLGJ040",
+    };
+    
   }
 
   static void addDioInterceptor() {
